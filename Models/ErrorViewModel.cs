6ada1f1a96182cc3e20a44277b49e9af44db18ca
KennyVell <kenneth.geme1@gmail.com>
2024-08-13 14:35:54 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace celsiaAssetsment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
